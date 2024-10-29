e64588011397e24c458e8f08af07795d8b803610
vigne <vigne@Vignesh>
2024-10-29 11:09:22 +0530

    hello

---
+++ C2TC_COREJAVA/src/com/tnsif/dayone/IdentifersDemo.java
@@ -0,0 +1,18 @@
+
+package  com.tnsif.dayone;
+
+public class IdentifersDemo { 
+
+public static void main(String[] args) { 
+
+ 
+int $number = 20; 
+System.out.println("value of the number variable is : "+ $number); 
+
+String studentName="Aniket";
+System.out.println("value is : "+ studentName); 
+ 
+
+} 
+
+}
---
+++ C2TC_COREJAVA/src/com/tnsif/daytwo/BreakDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.daytwo;
+
+public class BreakDemo {
+	public static void main(String[] args) { 
+	for (int i = 5; i < 10; i++) { 
+		   if (i == 6) 
+		    break; 
+		   System.out.println(i); 
+		  } 
+		 
+		 } 
+		} 
+		 
+		 
+
